06088c93619c928b708381c5a1a69fb580abe725
Jakob Fletcher <jakobfletcher1997@gmail.com>
2024-02-29 19:20:07 -0800

    Basic enemy AI added.

---
+++ Assets/Scripts/EnemyBasic.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBasic : MonoBehaviour
+{
+    public GameObject point1, point2;
+    private Rigidbody2D rb;
+    private Transform currentPosition;
+    public float speed;
+    void Start()
+    {
+        rb = GetComponent <Rigidbody2D>();
+        currentPosition = point2.transform;
+    }
+
+    void Update()
+    {
+        Vector2 point = currentPosition.position - transform.position;
+        if (currentPosition == point2.transform)
+        {
+            rb.velocity = new Vector2(speed, 0);
+        }
+        else 
+        {
+            rb.velocity = new Vector2(-speed, 0);
+        }
+        if (Vector2.Distance(transform.position, currentPosition.position) < 0.5f && currentPosition == point2.transform) 
+        {
+            currentPosition = point1.transform;
+        }
+        if (Vector2.Distance(transform.position, currentPosition.position) < 0.5f && currentPosition == point1.transform)
+        {
+            currentPosition = point2.transform;
+        }
+
+    }
+}
