f476a2e7a8e27337615bc122af1d07b3eb3c597d
Jakob Fletcher <jakobfletcher1997@gmail.com>
2024-02-29 17:46:55 -0800

    First commit. Basic player controls and camera movement.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private float jumpForce;
+    private float moveVelocity;
+    public bool isTouchingGround, isInvert, facingRight, isJumping, isFlipped;
+    public Transform groundCheckPoint;
+    public float groundCheckRadius;
+    public LayerMask groundLayer;
+    private Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        isTouchingGround = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, groundLayer);
+        moveVelocity = 0;
+        float horizontalMove = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveVelocity, rb.velocity.y);
+
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            moveVelocity = -speed;
+            //playerAnimator.SetFloat("speed", Mathf.Abs(horizontalMove));
+            if (isTouchingGround)
+            {
+                //playerAnimator.Play("Player_run");
+            }
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            moveVelocity = speed;
+            if (isTouchingGround)
+            {
+                //playerAnimator.Play("Player_run");
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && isTouchingGround == true)
+        {
+            isJumping = true;
+            //jumpTimeCounter = jumpTime;
+            if (isFlipped == true)
+            {
+                rb.velocity = Vector2.up * (-1) * jumpForce;
+                Debug.Log("FLIPPED");
+            }
+            else if (isFlipped == false)
+            {
+                rb.velocity = Vector2.up * jumpForce;
+            }
+        }
+        Flip(horizontalMove);
+        rb.velocity = new Vector2(moveVelocity, rb.velocity.y);
+
+    }
+    private void Flip(float horizontalMove)
+    {
+        if (horizontalMove > 0 && facingRight || horizontalMove < 0 && !facingRight)
+        {
+            facingRight = !facingRight;
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
+    }
+}
