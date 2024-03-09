b6d2591df2665d4d2bd32769889992f3426a803c
Jakob Fletcher <jakobfletcher1997@gmail.com>
2024-03-09 11:34:24 -0800

    Added background music with slowdown effect.

---
+++ Assets/Scripts/MusicScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicScript : MonoBehaviour
+{
+    public PlayerMovement playerReference;
+    public AudioSource themeMusic;
+
+    void FixedUpdate()
+    {
+        themeMusic.pitch = playerReference.pitchValue;
+    }
+}
