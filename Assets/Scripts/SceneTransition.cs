4b4297616d5c1f1436d1099b797acaec00336f7c
Jakob Fletcher <jakobfletcher1997@gmail.com>
2024-03-09 16:37:40 -0800

    Adding a scene transition.

---
+++ Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    public Animator transition;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return)) 
+        {
+            LoadNextLevel();
+        }       
+    }
+
+    public void LoadNextLevel() 
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+
+    }
+
+    IEnumerator LoadLevel(int levelIndex) 
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(0.25f);
+        SceneManager.LoadScene(levelIndex);
+    }
+
+}
